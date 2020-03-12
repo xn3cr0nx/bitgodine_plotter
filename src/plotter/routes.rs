57361197166eee3c6d7ff13c780fc4a67a430e42
Patrick Jusic <patrickjusic96@gmail.com>
2020-03-12 14:43:15 +0100

    Implement first working plot generator for timeline

--- src/plotter/routes.rs
+++ src/plotter/routes.rs
@@ -1,19 +1,74 @@
-use crate::api_error::ApiError;
-use actix_web::{web, HttpResponse};
-use serde_json::json;
-// use uuid::Uuid;
+extern crate plotly;
 
-async fn test() -> Result<HttpResponse, ApiError> {
-  Ok(HttpResponse::Ok().json(json!({ "message": "is working asshole".to_string() })))
+use actix_files::NamedFile;
+use actix_web::{web, HttpResponse};
+#[allow(unused_imports)]
+use plotly::common::{Line, LineShape, Marker, Mode};
+#[allow(unused_imports)]
+use plotly::{Histogram, NamedColor, Plot, Scatter};
+use serde::Deserialize;
+use serde_json::json;
+use slog::info;
+use std::path::Path;
+
+use crate::api_error::ApiError;
+use crate::AppState;
+
+use std::collections::HashMap;
+
+#[derive(Deserialize)]
+struct Coordinates {
+  x: Vec<f64>,
+  y: Vec<f64>,
 }
 
-pub async fn failtest() -> Result<HttpResponse, ApiError> {
-  Ok(HttpResponse::BadRequest().json(json!({ "message": "successfully failing".to_string() })))
+type Data = HashMap<String, Coordinates>;
+
+fn line_and_scatter_plot(
+  context: web::Data<AppState>,
+  data: web::Json<Data>,
+) -> Result<String, ApiError> {
+  let mut plot = Plot::new();
+
+  for (h, coordinates) in data.iter() {
+    info!(context.log, "generating plot for {}", h);
+    let x = coordinates.x.clone();
+    let y = coordinates.y.clone();
+    
+    let trace = Scatter::new(x, y)
+      .mode(Mode::LinesMarkers)
+      .name(h)
+      .show_legend(true)
+      .line(Line::new().shape(LineShape::Hv));
+    plot.add_trace(trace);
+  }
+
+  let filename = String::from("testfile.html");
+  let res = filename.clone();
+  plot.to_html(filename);
+  Ok(res)
+}
+
+async fn generate_plot(
+  context: web::Data<AppState>,
+  data: web::Json<Data>,
+) -> Result<NamedFile, ApiError> {
+  info!(context.log, "generating plot...");
+  let filename = line_and_scatter_plot(context, data).unwrap();
+  let path = Path::new(&filename);
+  Ok(NamedFile::open(path).unwrap())
+}
+
+async fn index() -> Result<HttpResponse, ApiError> {
+  Ok(
+    HttpResponse::Ok().json(json!({ "message": "Welcome to bitgodine_plotter v 0.1".to_string() })),
+  )
 }
 
 pub fn routes(cfg: &mut web::ServiceConfig) {
   cfg.service(
-    web::scope("/tests")
-      .route("/test", web::get().to(test))
-      .route("/failtest", web::get().to(failtest)));
+    web::scope("/plot")
+      .route("", web::post().to(generate_plot))
+      .route("", web::get().to(index)),
+  );
 }
